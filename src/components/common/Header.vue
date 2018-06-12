6fd7b961e22f0d29b1bd9786233f9ae6dd5a5486
wangyueweb <120400787@qq.com>
2018-06-12 13:26:22 +0800

    header
    Signed-off-by: wangyueweb <120400787@qq.com>

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -30,10 +30,7 @@
                         {{username}} <i class="el-icon-caret-bottom"></i>
                     </span>
                     <el-dropdown-menu slot="dropdown">
-                        <a href="https://github.com/wangyueweb" target="_blank">
-                            <el-dropdown-item>关于作者</el-dropdown-item>
-                        </a>
-                        <a href="https://github.com/wangyueweb" target="_blank">
+                        <a href="https://github.com/wangyueweb/vue-admin" target="_blank">
                             <el-dropdown-item>项目仓库</el-dropdown-item>
                         </a>
                         <el-dropdown-item divided  command="loginout">退出登录</el-dropdown-item>
